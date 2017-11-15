5a4896d57c55a254b5b48c6e5a93b3eaa1e869c8
fengke549015 <549015441@qq.com>
2017-11-15 22:52:32 +0800

    进行后台商品的添加

--- application/admin/controller/Goods.php
+++ application/admin/controller/Goods.php
@@ -17,21 +17,24 @@ class Goods extends AdminCommon
     }
     //图片上传的处理
     public function goods_pic(){
-        $aa=\request()->file('image');
-        dump($aa);
-        if($aa){
-            $info = $aa->move(ROOT_PATH . 'public' . DS . 'uploads');
+        $file=\request()->file('file');
+        if($file){
+            $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
             if($info){
                 // 成功上传后 获取上传信息
                 // 输出 jpg
-                echo $info->getExtension();
-                // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
-                echo $info->getSaveName();
-                // 输出 42a79759f284b767dfcb2a0197904287.jpg
-                echo $info->getFilename();
+//                echo $info->getExtension();
+//
+//                // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
+//                echo $info->getSaveName();
+//                // 输出 42a79759f284b767dfcb2a0197904287.jpg
+//                echo $info->getFilename();
+                $path =  $info->getSaveName();
+                return json(array('status'=>1,'path'=>$path,'msg'=>'图片上传成功'));
             }else{
                 // 上传失败获取错误信息
-                echo $aa->getError();
+//                echo $file->getError();
+                return json(array('status'=>0,'msg'=>'上传失败'));
             }
         }
     }
