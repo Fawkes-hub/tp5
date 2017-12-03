d3c82ef63810ffd445cef1eca5dee582efcf0cbb
fengke549015 <549015441@qq.com>
2017-12-03 15:34:49 +0800

    Merge branch 'master' of https://git.oschina.net/fengke549015/tp5
    # Conflicts:
    #	application/admin/controller/Goods.php
    #	application/admin/view/goods/edit.html

--- application/home/controller/Login.php
+++ application/home/controller/Login.php
@@ -1,9 +1,11 @@
 <?php
 namespace app\home\controller;
 
+use app\admin\model\User;
 use think\Controller;
 use think\Db;
 use think\Request;
+use think\Session;
 use ucpaas\Ucpaas;
 
 class Login extends Controller
@@ -18,42 +20,93 @@ class Login extends Controller
         $user_name=input('post.user_name');
         $user_pass=md5(input('post.user_pass'));
         //首先进行用户名判断
-        $res=Db::name('user')->where('user_name',$user_name)->value('user_pass');
-        if($res==$user_pass){
-            $this->success('登录成功','/');
+        $user=new User();
+        $pass=$user->where('user_name',$user_name)->value('user_pass');
+        $id=$user->where('user_name',$user_name)->value('id');
+        if($pass==$user_pass){
+            //登录成功来就存入Session;
+            Session::set('user_name',$user_name);
+            Session::set('user_id',$id);
+            $data=[
+                'status' =>0,
+                'msg'   =>'登录成功',
+            ];
         }else{
-            $this->error('用户名或密码错误，请重新输入');
+            $data=[
+                'status' =>1,
+                'msg'   =>'用户名或密码错误',
+            ];
         }
+        return json($data);
+    }
+    //退出
+    public function getLogout(){
+        Session::delete('user_name');
+        $this->redirect('/');
     }
     //注册
     public function getCreate(){
-
         return $this->fetch('login/create');
     }
     //保存用户
     public function postSave(Request $request){
         //先获取手机验证码，进行调用方法的验证
-        $phone=$request->only('user_phone');
-        //存入到属性中，进行调用
-        $this->phone=$phone['user_phone'];
+//        $phone=$request->only('user_phone');
+        $req=$request->except(['action','reuser_pass','phone_code','user_pass']);
+        $req['user_pass']=md5(input('post.user_pass'));
+        /*//存入到属性中，进行调用
+//        $this->phone=$phone['user_phone'];
         //实例化调用这个参数
-        $ucpass=$this->postPhoneCode();
+//        $ucpass=$this->postPhoneCode();
         //这个是存入属性的验证码内容
-        print_r($this->param);
+//        print_r($this->param);
         //最终获取得到的反馈数据，然后返回去进行页面的按钮变化
-        $ucpass=json_decode($ucpass);
-        //上面是欧佳俊接下来做的，我不做了
-
+//        $ucpass=json_decode($ucpass);
+        //上面是欧佳俊接下来做的，我不做了*/
         //如果验证码验证成功，进行下一步
-        $ucpass=1;
-        if($ucpass==input('post.phone_code')){
-
+        $code=1;
+        if($code != input('post.phone_code')){
+            //如果传过来的验证码不正确，就返回数据
+            $data=[
+                'status'=>1,
+                'msg'   =>'验证码不正确，请重新输入',
+            ];
+            return json($data);
         }
-
-       /* print_r($re);
-        print_r($this->phone);*/
+        //验证码正确的再进行下一步输出
+        $user=new \app\home\model\Login();
+        $save=$user->save($req);
+        if($save){
+            $data=[
+                'status' => 0,
+                'msg'    => '用户创建成功',
+            ];
+        }else{
+            $data=[
+                'status' => 2,
+                'msg'    => '用户创建失败，请稍后重试',
+            ];
+        }
+        return json($data);
     }
-
+    //用户名是否重复的检测
+    public function getUser(Request $request){
+        $user=$request->except('action');
+        $res=Db::name('user')->where($user)->find();
+        if($res){
+            $data=[
+                'status' => 1,
+                'msg'   => '用户名已存在，请重新输入',
+            ];
+        }else{
+            $data=[
+                'status' => 0,
+                'msg'   => '用户名可用',
+            ];
+        }
+        return json($data);
+    }
+    //验证码的结果调用
     public function postPhoneCode(){
         //获取传送过来的手机号
         $phone='+86'.$this->phone;
