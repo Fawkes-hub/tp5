6238292d016578061e8fd0a7d6ef5e364ad7122f
fengke549015 <549015441@qq.com>
2017-12-04 19:54:01 +0800

    公告和广告模块

--- application/home/controller/Index.php
+++ application/home/controller/Index.php
@@ -2,11 +2,22 @@
 namespace app\home\controller;
 
 use think\Controller;
+use think\Db;
 
 class Index extends Controller
 {
     public function index()
     {
+        $notice_datas=Db::table('tp_notices')->select();
+        //var_dump($notice_datas);
+        $i=0;
+        foreach($notice_datas as $v)
+        {
+            $notice_datas[$i]['create_time']=date('Y-m-d',$v['create_time']);
+            $notice_datas[$i]['update_time']=date('Y-m-d',$v['update_time']);
+            $i++;
+        }
+        $this->assign('notice_datas',$notice_datas);
         return $this->fetch('index/index');
     }
 
