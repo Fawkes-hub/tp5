b6845f71f260cb0a660b4ab756bc039bcc839f53
fengke549015 <549015441@qq.com>
2017-12-04 17:21:40 +0800

    友情链接

--- application/admin/controller/Friends.php
+++ application/admin/controller/Friends.php
@@ -1,6 +1,6 @@
 <?php
 namespace app\admin\controller;
-
+use think\Db;
 /**
  * Created by PhpStorm.
  * User: LENOVO
@@ -10,9 +10,43 @@ namespace app\admin\controller;
 use \app\admin\AdminCommon;
 class Friends extends AdminCommon{
     public function getindex(){
-        return 'aaaaa';
+        //加载友情链接
+        return $this->fetch("friends/index");
     }
-    public function getAA(){
-            echo "11111111";
+    //添加友情链接
+    public function getadd(){
+        return $this->fetch("friends/add");
     }
-}
+    //执行添加
+        public function postDoadd(){
+            //创建请求
+                $request=request();
+             //获取参数
+                 $friend=$request->except("action");
+            //判断传过去的值是否是空值
+                if(empty($name=$friend['firends_name'])){
+                    $this->error("请填写完整","friends/add");
+                 }else{
+                     if(empty($path=$friend['firends_path'])){
+                     $this->error("请填写完整","friends/add");
+                     }else {
+                         $a=substr($path, 0, 7);
+                         var_dump($path);
+                         var_dump($a);
+                         //判断是否以http://开头
+                         if ((substr($path, 0, 7) != 'http://') || (substr($path, 0, 8) != "https://")) {
+                             $paths = "http://" .$path;
+                         }
+                         $friend["firends_path"] = $paths;
+                     }
+                         //$friend["firends_path"] = $path;
+                         // $result=Db::table("tp_firends")->insert($friend);
+                         // $this->success("填写成功","friends/index");
+                         var_dump($friend);
+                     }
+                }
+
+
+
+        }
+
